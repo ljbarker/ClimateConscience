1cdeec1da14132b37bef15c2a7270835f22d8f3b
ljbarker <loganjb21@outlook.com>
2024-06-22 13:32:46 -0700

    Initialize Next.js app

---
+++ climate-conscience/src/calhacks/page.tsx
@@ -0,0 +1,7 @@
+export default function Hello() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <h1 className="text-4xl font-bold">Hello, world!</h1>
+        </div>
+    );
+}
