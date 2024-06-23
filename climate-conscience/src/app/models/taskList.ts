f3422bc7a956e81a6a4fda07f9dbe6ddbe153d22
ljbarker <loganjb21@outlook.com>
2024-06-23 13:23:02 -0700

    Change list type of Task list

--- climate-conscience/src/app/models/taskList.ts
+++ climate-conscience/src/app/models/taskList.ts
@@ -1,12 +1,11 @@
 import mongoose, { Schema } from 'mongoose';
-import Task from './task';
 
 export type TaskList = {
     tasks: string[];
 }
 
 const TaskListSchema = new Schema<TaskList>({
-    tasks: { type: [Task], required: true },
+    tasks: { type: [], required: true },
 });
 
 const TaskListModel = mongoose.models["TaskList"] || mongoose.model('TaskList', TaskListSchema);
